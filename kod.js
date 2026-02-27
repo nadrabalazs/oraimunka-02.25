75e6ab0c70193da5a56306e103fcc26a891e05af
nadrabalazs <158039410+nadrabalazs@users.noreply.github.com>
2026-02-27 10:30:23 +0100

    Add files via upload

--- kod.js
+++ kod.js
@@ -6,12 +6,16 @@ function visszaKep() {
     document.getElementById("nagykep").src = "ures.jpg";
 }
 
-function kivalaszt(melyikTermek) {
-    let hanyDarabotSzeretne = Number(prompt("Hány darabot szeretne?"));
-    if (hanyDarabotSzeretne > 0) 
-    {
-        let kosarbanUjTermek = document.createElement("li");
-        kosarbanUjTermek.textContent = hanyDarabotSzeretne + " db " + melyikTermek;
-        document.getElementById("kosar").appendChild(kosarbanUjTermek);
+function szamol() {
+    let sorok = document.getElementsByTagName("tr");
+    let vegosszeg = 0;
+    for (let i = 1; i < sorok.length; i++) {
+        let aktualisSor = sorok[i];
+        let ar = Number(aktualisSor.getElementsByClassName("ar")[0].innerHTML);
+        let db = Number(aktualisSor.getElementsByTagName("input")[0].value);
+        let reszosszeg = ar * db;
+        aktualisSor.getElementsByClassName("reszosszeg")[0].innerHTML = reszosszeg;
+        vegosszeg += reszosszeg;
     }
+    document.getElementById("osszertek").innerHTML = vegosszeg;
 }
